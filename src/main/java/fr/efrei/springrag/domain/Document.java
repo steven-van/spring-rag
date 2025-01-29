fe7b178787f5d0b0227f1e9adb8d6615a747754e
Bastien <bastien@lacour75.com>
2025-01-29 13:47:39 +0100

    step kubernetes

---
+++ src/main/java/fr/efrei/springrag/domain/Document.java
@@ -0,0 +1,15 @@
+package fr.efrei.springrag.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Document {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(nullable = false)
+    private Long id;
+    private String title;
+    private String description;
+    private String author;
+    private String publisher;
+}
--- src/main/java/fr/efrei/springrag/web/rest/SampleResource.java
+++ src/main/java/fr/efrei/springrag/web/rest/SampleResource.java
@@ -1,5 +1,6 @@
 package fr.efrei.springrag.web.rest;
 
+import fr.efrei.springrag.web.dto.Sample;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,4 +13,8 @@ public class SampleResource {
         return "Hello " + value + "!";
     }
 
+    @GetMapping("/samples/dto/{value}")
+    public Sample helloJson(@PathVariable(value = "value", required = false) final String value) {
+        return new Sample("Hello " + value + "!");
+    }
 }
